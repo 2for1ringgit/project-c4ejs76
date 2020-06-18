375e39a1bb7fd8405d4c7b763b4450ecfc7f169e
2for1ringgit <silvercrane1998@gmail.com>
2020-06-18 12:14:48 +0700

    test

--- Project C4E/app.js
+++ Project C4E/app.js
@@ -4,11 +4,24 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   modal.style.display = "block";
 }
+
 span.onclick = function() {
   modal.style.display = "none";
 }
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
   }
 }
