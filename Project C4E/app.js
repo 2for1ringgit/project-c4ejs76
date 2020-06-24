f2036f07d714b64aa3a1eb8800f5d8fe1d0bfa67
2for1ringgit <silvercrane1998@gmail.com>
2020-06-24 16:49:04 +0700

    sss

--- Project C4E/app.js
+++ Project C4E/app.js
@@ -1,55 +1,58 @@
+// window.onscroll = function() {myFunction()};
 
-// var slideIndex = 1;
-// showSlides(slideIndex);
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-// //auto slide
-// var slideIndex = 0;
-// showSlides();
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
 
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
 //   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 4000); // Change image every 2 seconds
 // }
+let menu_item = document.getElementsByClassName("menu-item");
+menu_item.onclick = function addClass(){
+  menu_item.classList.add("cl-green");
+}
 
-// let add_mua = document.getElementsByClassName("datmua");
-// for (let i = 0; i < add_mua.length; i++) {
-//     add = add_mua[i];
-//     add.addEventListener("click", function (event) {
-//         let button = event.target;
-//         let product = button.parentElement.parentElement;
-//         let title = product.getElementsByClassName("name")[0].innerText;
-//         let price = product.getElementsByClassName("gia")[0].innerText;
-//         additemtobuy(title, price);
-//         updatebuy();
-//     });
-// };
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); 
+}
 
 let name_alert = document.querySelector(".checkout-name");
 let number_alert = document.querySelector(".checkout-number");
@@ -104,19 +107,6 @@ checkout_btn.addEventListener("click", function showAlert() {
     location.reload();
   }
 });
-
-// if (! name_alert.value ) {
-//   checkout_btn.onclick = alert ("Tên không thể bỏ trống");
-// }
-// else if (!name_alert.value ) {
-//   checkout_btn.onclick = alert ("Tên không thể bỏ trống");
-// }
-// else if (name_alert.value == "") {
-//   checkout_btn.onclick = alert ("Tên không thể bỏ trống");
-
-// else {
-//   checkout_btn.onclick = alert ("Bạn đã đặt hàng thành công");
-// }
 
 let cart = [];
 function showData() {
